46585d9179cf4e1c6a36187194c9e421b1453043
AyushKr25 <https://github.com/AyushKr25>
2026-04-24 15:16:43 +0530

    Ayush Added UC4

--- App/src/QuantityMeasurementApp.java
+++ App/src/QuantityMeasurementApp.java
@@ -1,51 +1,81 @@
-public class QuantityMeasurementApp {
+enum LengthUnit {
+    FEET(12.0),
+    INCHES(1.0),
+    YARDS(36.0),
+    CENTIMETERS(0.393701);
 
-    enum LengthUnit {
-        FEET(1.0),
-        INCH(1.0 / 12.0);
+    private final double conversionFactor;
 
-        private final double factor;
-
-        LengthUnit(double factor) {
-            this.factor = factor;
-        }
-
-        public double toFeet(double value) {
-            return value * factor;
-        }
+    LengthUnit(double conversionFactor) {
+        this.conversionFactor = conversionFactor;
     }
 
-    static class QuantityLength {
-        private final double value;
-        private final LengthUnit unit;
+    public double getConversionFactor() {
+        return conversionFactor;
+    }
+}
 
-        public QuantityLength(double value, LengthUnit unit) {
-            this.value = value;
-            this.unit = unit;
-        }
+class Length {
+    private final double value;
+    private final LengthUnit unit;
 
-        private double toFeet() {
-            return unit.toFeet(value);
-        }
+    public Length(double value, LengthUnit unit) {
+        this.value = value;
+        this.unit = unit;
+    }
 
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj == null || getClass() != obj.getClass()) return false;
-            QuantityLength other = (QuantityLength) obj;
-            return Double.compare(this.toFeet(), other.toFeet()) == 0;
+    private double convertToBaseUnit() {
+        double inInches = this.value * this.unit.getConversionFactor();
+        return Math.round(inInches * 100.0) / 100.0;
+    }
+
+    public boolean compare(Length thatLength) {
+        if (thatLength == null || this.unit == null || thatLength.unit == null) {
+            return false;
         }
+        return Double.compare(this.convertToBaseUnit(), thatLength.convertToBaseUnit()) == 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Length length = (Length) o;
+        return compare(length);
+    }
+}
+
+public class QuantityMeasurementApp {
+
+    public static boolean demonstrateLengthEquality(Length length1, Length length2) {
+        return length1.equals(length2);
+    }
+
+    public static boolean demonstrateLengthComparison(double value1, LengthUnit unit1, double value2, LengthUnit unit2) {
+        Length length1 = new Length(value1, unit1);
+        Length length2 = new Length(value2, unit2);
+        boolean result = length1.equals(length2);
+        System.out.println("Are lengths equal? " + result);
+        return result;
     }
 
     public static void main(String[] args) {
-        QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
-        QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCH);
+        demonstrateLengthComparison(1.0, LengthUnit.FEET, 12.0, LengthUnit.INCHES);
+        demonstrateLengthComparison(1.0, LengthUnit.YARDS, 36.0, LengthUnit.INCHES);
+        demonstrateLengthComparison(100.0, LengthUnit.CENTIMETERS, 39.3701, LengthUnit.INCHES);
+        demonstrateLengthComparison(3.0, LengthUnit.FEET, 1.0, LengthUnit.YARDS);
+        demonstrateLengthComparison(30.48, LengthUnit.CENTIMETERS, 1.0, LengthUnit.FEET);
 
-        System.out.println(q1.equals(q2));
+        Length length1 = new Length(1.0, LengthUnit.FEET);
+        Length length2 = new Length(12.0, LengthUnit.INCHES);
+        System.out.println("Are lengths equal? " + length1.equals(length2));
 
-        QuantityLength q3 = new QuantityLength(1.0, LengthUnit.INCH);
-        QuantityLength q4 = new QuantityLength(1.0, LengthUnit.INCH);
+        Length length3 = new Length(1.0, LengthUnit.YARDS);
+        Length length4 = new Length(36.0, LengthUnit.INCHES);
+        System.out.println("Are lengths equal? " + length3.equals(length4));
 
-        System.out.println(q3.equals(q4));
+        Length length5 = new Length(100.0, LengthUnit.CENTIMETERS);
+        Length length6 = new Length(39.3701, LengthUnit.INCHES);
+        System.out.println("Are lengths equal? " + length5.equals(length6));
     }
 }
